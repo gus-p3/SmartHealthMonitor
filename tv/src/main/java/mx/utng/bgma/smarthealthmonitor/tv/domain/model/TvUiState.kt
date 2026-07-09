bb81d49cefd4ccd25014e687e8e42070888b3eff
gus-p3 <sinnamexd3432@gmail.com>
2026-07-08 19:19:19 -0600

    feat(tv): add TvUiState domain model

---
+++ tv/src/main/java/mx/utng/bgma/smarthealthmonitor/tv/domain/model/TvUiState.kt
@@ -0,0 +1,9 @@
+package mx.utng.bgma.smarthealthmonitor.tv.domain.model
+import mx.edu.utng.bgma.smarthealthmonitor.data.db.LecturaFC
+ 
+data class TvUiState(
+    val lecturas    : List<LecturaFC> = emptyList(),
+    val fcActual    : Int             = 0,
+    val isLoading   : Boolean         = true,
+    val error       : String?         = null,
+)
